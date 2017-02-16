6da113dc28649d436050d7ae7f6c7c11f7a64b92
nwuensche <Niklas.Wuensche@mailbox.tu-dresden.de>
2017-02-16 18:08:54 +0100

    Show date in title

--- app/src/main/java/journal/app/niklas/a5minutejournal/MainActivity.kt
+++ app/src/main/java/journal/app/niklas/a5minutejournal/MainActivity.kt
@@ -3,6 +3,8 @@ package journal.app.niklas.a5minutejournal
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,6 +13,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         setUpTabs()
+        setUpActionBar()
 
     }
 
@@ -19,6 +22,12 @@ class MainActivity : AppCompatActivity() {
         tabhost.addTab(tabhost.newTabSpec("tab1").setIndicator("ALL ENTRIES").setContent(R.id.entries))
         tabhost.addTab(tabhost.newTabSpec("tab2").setIndicator("TODAY").setContent(R.id.today))
         tabhost.setCurrentTabByTag("tab2")
+    }
+
+
+    private fun setUpActionBar() {
+        val today: Date = Calendar.getInstance().time
+        supportActionBar?.title = SimpleDateFormat("dd MMMM yyyy", Locale.ENGLISH).format(today.time)
     }
 
 }
