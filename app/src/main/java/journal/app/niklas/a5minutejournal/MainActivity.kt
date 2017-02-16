87c3028e8e850d0b63dc7d0d501c9d597c7ca508
nwuensche <Niklas.Wuensche@mailbox.tu-dresden.de>
2017-02-16 17:50:29 +0100

    SetUp Tabs, Remove hello world text

--- app/src/main/java/journal/app/niklas/a5minutejournal/MainActivity.kt
+++ app/src/main/java/journal/app/niklas/a5minutejournal/MainActivity.kt
@@ -2,11 +2,23 @@ package journal.app.niklas.a5minutejournal
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setUpTabs()
+
     }
+
+    private fun setUpTabs() {
+        tabhost.setup()
+        tabhost.addTab(tabhost.newTabSpec("tab1").setIndicator("ALL ENTRIES").setContent(R.id.entries))
+        tabhost.addTab(tabhost.newTabSpec("tab2").setIndicator("TODAY").setContent(R.id.today))
+        tabhost.setCurrentTabByTag("tab2")
+    }
+
 }
