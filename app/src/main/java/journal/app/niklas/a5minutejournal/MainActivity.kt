daf46b46a4a049093c820499b2e713d4708fc548
nwuensche <Niklas.Wuensche@mailbox.tu-dresden.de>
2017-02-18 21:36:19 +0100

    Add Functions to read and write files with proper file name

--- app/src/main/java/journal/app/niklas/a5minutejournal/MainActivity.kt
+++ app/src/main/java/journal/app/niklas/a5minutejournal/MainActivity.kt
@@ -1,5 +1,6 @@
 package journal.app.niklas.a5minutejournal
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.app.ActionBar
@@ -9,6 +10,10 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.ab_today_title.*
 import java.text.SimpleDateFormat
 import java.util.*
+import android.content.Context.MODE_PRIVATE
+import java.io.File
+import java.io.FileOutputStream
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -32,12 +37,33 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
         supportActionBar?.setCustomView(R.layout.ab_today_title)
 
+        ab_today_title.text = getToday()
+    }
+
+    private fun getToday(): String {
         val today: Date = Calendar.getInstance().time
-        ab_today_title.text = SimpleDateFormat("dd MMMM yyyy", Locale.ENGLISH).format(today.time)
+        return SimpleDateFormat("dd MMMM yyyy", Locale.ENGLISH).format(today.time)
     }
 
     fun onTodaySaveButton(v: View) {
-        Log.e("click", "click")
+        val filename = getToday().replace(" ", "_").plus(".txt")
+        val string = "Hello world!"
+
+        try {
+            val outputStream: FileOutputStream = openFileOutput(filename, Context.MODE_PRIVATE)
+            outputStream.write(string.toByteArray())
+            outputStream.close()
+        }
+        catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    fun getAllDateFileNames(): List<String> {
+        return filesDir
+                .listFiles()
+                .map { it.name }
+                .filter { Character.isDigit(it[0]) } // 9 Feb -> 09 February
     }
 
 }
