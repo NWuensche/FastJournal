50f574001cb0c379efe5fd0ae88d5c496a05c3af
nwuensche <Niklas.Wuensche@mailbox.tu-dresden.de>
2017-02-19 20:11:38 +0100

    Save and Load files

--- app/src/main/java/journal/app/niklas/a5minutejournal/LoadFiles.kt
+++ app/src/main/java/journal/app/niklas/a5minutejournal/LoadFiles.kt
@@ -1,5 +1,8 @@
 package journal.app.niklas.a5minutejournal
 
+import java.io.File
+import java.io.FileInputStream
+
 /**
  * Created by nwuensche on 19.02.17.
  */
@@ -10,6 +13,24 @@ object LoadFiles {
                 .listFiles()
                 .map { it.name }
                 .filter { Character.isDigit(it[0]) } // 9 Feb -> 09 February
+    }
+
+    fun getTextFromFile(activity: MainActivity, fileName: String): String {
+        var buffer = ByteArray(0)
+
+        try {
+            val file = File(activity.baseContext.filesDir, fileName)
+            buffer = ByteArray(file.length().toInt())
+
+            val inputStream: FileInputStream = FileInputStream(file)
+            inputStream.read(buffer)
+            inputStream.close()
+        }
+        catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+        return String(buffer)
     }
 
 }
--- app/src/main/java/journal/app/niklas/a5minutejournal/SaveFiles.kt
+++ app/src/main/java/journal/app/niklas/a5minutejournal/SaveFiles.kt
@@ -1,31 +1,51 @@
 package journal.app.niklas.a5minutejournal
 
 import android.content.Context
-import android.util.Log
 import journal.app.niklas.a5minutejournal.LoadFiles.getAllDateFileNames
+import kotlinx.android.synthetic.main.fragment_today.*
 import java.io.FileOutputStream
+
 
 /**
  * Created by nwuensche on 19.02.17.
  */
 object SaveFiles {
 
-    fun onSaveToday(activity: MainActivity) {
+    private var activity: MainActivity? = null
+
+    fun onSaveToday(mainActivity: MainActivity) {
+        activity = mainActivity
+
         val filename = Today.getToday().replace(" ", "_").plus(".txt")
-        val string = "Hello world!"
+        val input = getAllInput()
 
         try {
-            val outputStream: FileOutputStream = activity.openFileOutput(filename, Context.MODE_PRIVATE)
-            outputStream.write(string.toByteArray())
+            val outputStream: FileOutputStream = activity!!.openFileOutput(filename, Context.MODE_PRIVATE)
+            outputStream.write(input.toByteArray())
             outputStream.close()
         }
         catch (e: Exception) {
             e.printStackTrace()
         }
 
-        getAllDateFileNames(activity)
-                .forEach { Log.e("test", it) }
+        getAllDateFileNames(activity!!)
+                .forEach { println("Filename: $it:\n".plus(LoadFiles.getTextFromFile(activity!!, it))) }
+    }
 
+    private fun getAllInput(): String {
+        val grateful: String = getGrateful()
+
+        val input = grateful
+
+        return input
+    }
+
+    private fun getGrateful(): String {
+        val grateful1 = activity!!.editText_grateful1.text.toString().plus('\n')
+
+        val grateful = grateful1.plus("\n")
+
+        return grateful
     }
 
 }
