f9cbc326e4e816df197e2dc48bfeb45d952a41ca
nwuensche <Niklas.Wuensche@mailbox.tu-dresden.de>
2017-02-20 16:45:48 +0100

    Save all daily stuff

--- app/src/main/java/journal/app/niklas/a5minutejournal/SaveFiles.kt
+++ app/src/main/java/journal/app/niklas/a5minutejournal/SaveFiles.kt
@@ -1,6 +1,7 @@
 package journal.app.niklas.a5minutejournal
 
 import android.content.Context
+import android.util.Log
 import journal.app.niklas.a5minutejournal.LoadFiles.getAllDateFileNames
 import kotlinx.android.synthetic.main.fragment_today.*
 import java.io.FileOutputStream
@@ -29,23 +30,46 @@ object SaveFiles {
         }
 
         getAllDateFileNames(activity!!)
-                .forEach { println("Filename: $it:\n".plus(LoadFiles.getTextFromFile(activity!!, it))) }
+                .forEach { Log.e("test", "Filename: $it:\n".plus(LoadFiles.getTextFromFile(activity!!, it))) }
     }
 
     private fun getAllInput(): String {
         val grateful: String = getGrateful()
+        val great: String = getGreat()
+        val daily: String = getDaily()
 
-        val input = grateful
+        val input = "$grateful$great$daily"
 
         return input
     }
 
     private fun getGrateful(): String {
         val grateful1 = activity!!.editText_grateful1.text.toString().plus('\n')
+        val grateful2 = activity!!.editText_grateful2.text.toString().plus('\n')
+        val grateful3 = activity!!.editText_grateful3.text.toString().plus('\n')
 
-        val grateful = grateful1.plus("\n")
+        val grateful = "$grateful1$grateful2$grateful3\n"
 
         return grateful
+    }
+
+    private fun getGreat(): String {
+        val great1 = activity!!.editText_great1.text.toString().plus('\n')
+        val great2 = activity!!.editText_great2.text.toString().plus('\n')
+        val great3 = activity!!.editText_great3.text.toString().plus('\n')
+
+        val great = "$great1$great2$great3\n"
+
+        return great
+    }
+
+    private fun getDaily(): String {
+        val daily1 = activity!!.editText_daily1.text.toString().plus('\n')
+        val daily2 = activity!!.editText_daily2.text.toString().plus('\n')
+
+        val great = "$daily1$daily2\n"
+
+        return great
     }
 
 }
