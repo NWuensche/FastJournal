c83202c9b86d504326df0ef8c434f60c4efb77d4
nwuensche <Niklas.Wuensche@mailbox.tu-dresden.de>
2017-02-22 20:52:29 +0100

    TODOs, Layout add

--- app/src/main/java/journal/app/niklas/a5minutejournal/Today.kt
+++ app/src/main/java/journal/app/niklas/a5minutejournal/Today.kt
@@ -11,7 +11,7 @@ object Today {
 
     fun getDate(): String {
         val cal: Calendar = Calendar.getInstance()
-        //cal.set(2017, 1, 1)
+        //cal.set(2017, 1, 7)
         val today: Date = cal.time
         return SimpleDateFormat("dd MMMM yyyy", Locale.ENGLISH).format(today.time)
     }
