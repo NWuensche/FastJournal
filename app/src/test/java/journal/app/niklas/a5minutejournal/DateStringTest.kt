02208181bdc4f4902f9a3e1e5b4b56d0d5bf95ca
nwuensche <Niklas.Wuensche@mailbox.tu-dresden.de>
2017-03-10 16:21:35 +0100

    Add dateString compareTo tests

--- app/src/test/java/journal/app/niklas/a5minutejournal/DateStringTest.kt
+++ app/src/test/java/journal/app/niklas/a5minutejournal/DateStringTest.kt
@@ -1,10 +1,11 @@
 package journal.app.niklas.a5minutejournal
 
 import org.hamcrest.CoreMatchers.`is`
-import org.hamcrest.Matchers.lessThan
+import org.hamcrest.Matchers.*
 
 import org.junit.Assert.*
 import org.junit.Test
+import java.util.*
 
 /**
  * Created by nwuensche on 10.03.17.
@@ -13,9 +14,14 @@ class DateStringTest {
 
     @Test
     fun compareRight() {
-        val first: DateString = DateString("23 January 2013")
-        val second: DateString = DateString("24 January 2013")
+        val first: DateString = DateString("23 February 2013")
+        val second: DateString = DateString("24 February 2013")
+        val third: DateString = DateString("22 March 2013")
+        val forth: DateString = DateString("21 July 2014")
 
         assertThat(first.compareTo(second), `is`(lessThan(0)))
+        assertThat(third.compareTo(first), `is`(greaterThan(0)))
+        assertThat(second.compareTo(forth), `is`(lessThan(0)))
+
     }
 }
